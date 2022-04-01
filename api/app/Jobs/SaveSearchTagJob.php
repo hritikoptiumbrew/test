dedd9ab537c83f1bf9c99b8628f40a32a3064382
JAY PARMAR <jayparmar.optimumbrew@gmail.com>
2022-04-01 18:40:43 +0530

    1. Remove log in SaveSearchTagJob.

--- api/app/Jobs/SaveSearchTagJob.php
+++ api/app/Jobs/SaveSearchTagJob.php
@@ -38,7 +38,7 @@ class SaveSearchTagJob implements ShouldQueue
             $this->is_success = $is_success;
             $this->is_featured = $is_featured;
             $this->category_id = $category_id;
-            Log::info('SaveSearchTagJob: Value', ['template_count' => $this->template_count , 'search_tag' => $this->search_category , 'sub_category_id' => $this->sub_category_id , 'is_success' => $this->is_success , 'is_featured' => $this->is_featured , 'category_id' => $this->category_id]);
+
         } catch (Exception $e) {
             Log::error("SaveSearchTagJob construct() : ", ["Exception" => $e->getMessage(), "\nTraceAsString" => $e->getTraceAsString()]);
         }
@@ -63,28 +63,14 @@ class SaveSearchTagJob implements ShouldQueue
                                             category_id = ? AND 
                                             sub_category_id = ?',[$this->search_category, $this->is_success, $this->is_featured, $this->category_id, $this->sub_category_id]);
 
-            Log::info("SaveSearchTagJob: Tag is exist or not.", ['tag_result' => $tag_details, 'search_tag' => $this->search_category, 'is_success' => $this->is_success, 'is_featured' => $this->is_featured, 'category_id' => $this->category_id, 'sub_category_id' => $this->sub_category_id]);
-
             if(count($tag_details) > 0) {
                 $id = $tag_details[0]->id;
-                $update_query = DB::update('UPDATE tag_analysis_master SET search_count = search_count + 1, content_count = ? WHERE id = ?',[$this->template_count, $id]);
-
-                if (!$update_query) {
-                    Log::info('SaveSearchTagJob : Search tag count is not increase', ["id" => $id]);
-                }else{
-                    Log::info('SaveSearchTagJob : Search tag count is increase', ["result" => $update_query]);
-                }
+                DB::update('UPDATE tag_analysis_master SET search_count = search_count + 1, content_count = ? WHERE id = ?',[$this->template_count, $id]);
 
             } else {
-                $insert_query = DB::insert('INSERT INTO
+                DB::insert('INSERT INTO
                                tag_analysis_master(tag, is_success, is_featured, category_id, content_count, search_count, sub_category_id)
                                VALUES(?, ?, ?, ?, ?, ?, ?)', [$this->search_category, $this->is_success, $this->is_featured, $this->category_id, $this->template_count, 1, $this->sub_category_id]);
-
-                if (!$insert_query) {
-                    Log::info('SaveSearchTagJob : Search tag not insert');
-                }else{
-                    Log::info('SaveSearchTagJob : Search tag insert', ["result" => $insert_query]);
-                }
             }
 
         } catch (Exception $e) {
