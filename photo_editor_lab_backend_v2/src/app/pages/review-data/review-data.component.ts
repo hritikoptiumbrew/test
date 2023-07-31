d138d35caf9cbd1312857dbb28b9ae6d1922eee1
Parth <parth@gmail.com>
2023-07-31 09:45:59 +0530

    1. Added review data in photoeditlab.

--- photo_editor_lab_backend_v2/src/app/pages/review-data/review-data.component.ts
+++ photo_editor_lab_backend_v2/src/app/pages/review-data/review-data.component.ts
@@ -15,14 +15,14 @@ import { ERROR, ENV_CONFIG } from '../../app.constants';
 })
 export class ReviewDataComponent implements OnInit {
   token: any;
-  reviewArray: any;
+  reviewArray: any ;
   totalRecord : any;
   sortByTagName: any;
   order_type: boolean;
   order_type_val: string;
   currentPage:any = 1;
   pageSize: any = [25,50,75,100];
-  selectedImageSize:any = '25';
+  selectedSize:any = '25';
   previousLabel = "<";
   nextLabel=">";
 
@@ -35,7 +35,7 @@ export class ReviewDataComponent implements OnInit {
   }
 
   setPageSize(value: any) {
-    this.selectedImageSize = value;
+    this.selectedSize = value;
     this.getapidata();
   }
   
@@ -59,24 +59,29 @@ export class ReviewDataComponent implements OnInit {
 
   getapidata(){
     this.utils.showPageLoader();
-    this.dataService.postData('getAiChats',{},{
+    this.dataService.postData('getAiChats',{
+        "item_count": this.selectedSize,
+        "page": this.currentPage,
+        "order_by": this.sortByTagName,
+        "order_type": this.order_type_val
+    },{
       headers: {
         'Authorization': 'Bearer ' + this.token
       }
     }).then((results: any) => {
       if (results.code == 200) {
-        for(let i=0; i<=results.data.length-1; i++){
-          results.data[i].app_json = JSON.parse(results.data[i].app_json);
-          results.data[i].device_json =JSON.parse(results.data[i].device_json);
-          if(results.data[i].app_json == null){
-            results.data[i].app_json = {"app_version" : "","platform" : ""}
+        for(let i=0; i<=results.data.ai_chats.length-1; i++){
+          results.data.ai_chats[i].app_json = JSON.parse(results.data.ai_chats[i].app_json);
+          results.data.ai_chats[i].device_json =JSON.parse(results.data.ai_chats[i].device_json);
+          if(results.data.ai_chats[i].app_json == null){
+            results.data.ai_chats[i].app_json = {"app_version" : "","platform" : ""}
           }
-          if(results.data[i].device_json == null){
-            results.data[i].device_json = {"device_country_code" : "","device_language" : ""}
+          if(results.data.ai_chats[i].device_json == null){
+            results.data.ai_chats[i].device_json = {"device_country_code" : "","device_language" : ""}
           }
         } 
-        this.reviewArray = results.data;  
-        this.totalRecord = results.data.length;
+        this.reviewArray = results.data.ai_chats;  
+        this.totalRecord = results.data.total_row;
         this.utils.hidePageLoader();
       }else if (results.code == 201) {
         this.utils.showError(results.message, 4000);
